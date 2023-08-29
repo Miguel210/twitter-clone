bf75fecb36e92e7271455485f2154472a07ffb4b
Miguel Angel <mickyjunior04@gmail.com>
2023-08-29 17:11:57 -0600

    constants

--- src/likes.rs
+++ src/likes.rs
@@ -1,12 +1,12 @@
 use actix_web::{get,post,delete,HttpResponse};
 use actix_web::web::Path;
-
+use crate::constants::APPLICATION_JSON;
 #[get("/tweets/{id}/likes")]
 pub async fn get_likes_by_tweet(path: Path<(String,)>) -> HttpResponse {
     let likes = 100;
 
     HttpResponse::Ok()
-    .content_type("application/json")
+    .content_type(APPLICATION_JSON)
     .json(likes)
 }
 
@@ -16,7 +16,7 @@ pub async fn like_tweet(path: Path<(String,)>) -> HttpResponse {
     let like = "ok";
 
     HttpResponse::Created()
-    .content_type("application/json")
+    .content_type(APPLICATION_JSON)
     .json(like)
 }
 
@@ -24,7 +24,7 @@ pub async fn like_tweet(path: Path<(String,)>) -> HttpResponse {
 pub async fn remove_likes(path: Path<(String,)>) -> HttpResponse {
 
     HttpResponse::NoContent()
-    .content_type("application/json")
+    .content_type(APPLICATION_JSON)
     .await
     .unwrap()
 }
--- src/tweets.rs
+++ src/tweets.rs
@@ -1,5 +1,6 @@
 use actix_web::{get,post,HttpResponse};
 use actix_web::web::Path;
+use crate::constants::APPLICATION_JSON;
 
 //api/tweets
 #[get("/tweets")]
@@ -7,7 +8,7 @@ pub async fn get_tweets() -> HttpResponse {
     let tweets = ["tweet 1: hola","tweet 2: chao"];
 
     HttpResponse::Ok()
-    .content_type("application/json")
+    .content_type(APPLICATION_JSON)
     .json(tweets)
 }
 
@@ -16,7 +17,7 @@ pub async fn create_tweet() -> HttpResponse {
     let nuevo_tweet = "Este es un nuevo tweet";
 
     HttpResponse::Created()
-    .content_type("application/json")
+    .content_type(APPLICATION_JSON)
     .json(nuevo_tweet)
 }
 
@@ -25,6 +26,6 @@ pub async fn get_tweet_by_id(path: Path<(String,)>) -> HttpResponse {
     let tweet = format!("este es el tweet {:?}", path.0);
 
     HttpResponse::Ok()
-    .content_type("application/json")
+    .content_type(APPLICATION_JSON)
     .json(tweet)
 }
