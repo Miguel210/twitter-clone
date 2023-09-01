0949feb62d22650553f9bf8597a786fba43bee45
Miguel Angel <mickyjunior04@gmail.com>
2023-09-01 00:03:22 -0600

    add database

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use actix_web::{App,HttpServer};
-
+use std::env;
+use diesel::r2d2::{ConnectionManager,Pool};
+use diesel::PgConnection;
 mod tweets;
 mod likes;
 mod constants;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new( || {
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL NOT FOUND");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = Pool::builder().build(manager).expect("could not create pool");
+
+    HttpServer::new( move || {
         App::new()
+        .data(pool.clone())
         .service(tweets::get_tweets)
         .service(tweets::create_tweet)
         .service(tweets::get_tweet_by_id)
